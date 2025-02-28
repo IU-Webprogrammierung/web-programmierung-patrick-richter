e083e3ec879c86c50157213993c79a72219cd691
Patrick Richter <p.t.richter@gmail.com>
2025-02-28 10:55:18 +0100

    Hiding overlay after Escape and click into overlay-left added; aria handling adapted

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let overlayStatus = false;
-
 document.querySelector("#openOverlay").addEventListener("click", showOverlay);
 document.querySelector("#closeOverlay").addEventListener("click", hideOverlay);
 document.querySelector("#overlayLeft").addEventListener("click", hideOverlay);
-
+addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    hideOverlay();
+  }
+});
 
 // Öffnet das Overlay und entfernt die Schließen-Animation
-
 
 function showOverlay() {
   let overlay = document.querySelector(".overlay");
@@ -15,27 +16,35 @@ function showOverlay() {
 
   if (!overlay.classList.contains("show-overlay")) {
     overlay.classList.add("show-overlay");
-    overlayStatus = true;
-    console.log("Overlay wird angezeigt");
-  }     
-}
-
-// schließt das Overlay und wartet bis Animationen abgeschlossen sind
-// TODO: Escape Taste zum Schließen und Schließen bei Klick ins linke Fenster
-
-function hideOverlay() {
-    let overlay = document.querySelector(".overlay");
-    let overlayRight = document.querySelector(".overlay-right");
-
-  if (overlay.classList.contains("show-overlay") && !overlay.classList.contains("hiding")) {
-    overlay.classList.add("closing");
-    overlayRight.addEventListener("transitionend", function() {
-        overlay.classList.remove("show-overlay");
-        overlay.classList.remove("closing");
-        }, {once:true})
-    overlayStatus = false;
-    console.log("Overlay wird nicht angezeigt");
+    overlay.setAttribute("aria-hidden", "false");
+    console.log("Overlay visible");
+    console.log(document.activeElement);
   }
 }
 
+// schließt das Overlay und wartet bis Animationen abgeschlossen sind
 
+function hideOverlay() {
+  let overlay = document.querySelector(".overlay");
+
+  if (
+    overlay.classList.contains("show-overlay") &&
+    !overlay.classList.contains("hiding")
+  ) {
+    let overlayRight = document.querySelector(".overlay-right");
+
+    overlay.classList.add("closing");
+    overlayRight.addEventListener(
+      "transitionend",
+      function () {
+        overlay.classList.remove("show-overlay");
+        overlay.classList.remove("closing");
+        overlay.setAttribute("aria-hidden", "true");
+        document.querySelector("#openOverlay").focus();
+        console.log("Overlay hidden");
+        console.log(document.activeElement);
+      },
+      { once: true }
+    );
+  }
+}
