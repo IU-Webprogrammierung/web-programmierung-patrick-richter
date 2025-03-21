262740360f030df1fad1e7390fca3b91ec6f1141
Patrick Richter <p.t.richter@gmail.com>
2025-03-21 17:24:40 +0100

    Footer API included and dataStore uipdated accordingly

--- js/core/dataStore.js
+++ js/core/dataStore.js
@@ -12,6 +12,7 @@ const dataStore = {
   projectsData: null,
   aboutImprintData: null,
   clientsData: null,
+  footerData: null,
 
   getProjects: function () {
     return this.projectsData;
@@ -25,25 +26,31 @@ const dataStore = {
     return this.clientsData;
   },
 
+  getFooter: function () {
+    return this.footerData;
+  },
+
   loadData: async function () {
     try {
       console.log("dataStore: Daten-Fetch beginnt...");
 
-      // Alle drei externen Inputs laden
-      const [projectsResponse, aboutResponse, clientsResponse] =
+      // Alle vier externen Inputs laden
+      const [projectsResponse, aboutResponse, clientsResponse, footerResponse] =
         await Promise.all([
           fetch("content/projects.json"),
           fetch("content/aboutImprint.json"),
           fetch("content/clients.json"),
+          fetch("content/footer.json")
         ]);
 
       // Überprüfen, ob alle Responses erfolgreich waren
-      if (!projectsResponse.ok || !aboutResponse.ok || !clientsResponse.ok) {
+      if (!projectsResponse.ok || !aboutResponse.ok || !clientsResponse.ok || !footerResponse.ok) {
         console.error(
           "Fehler beim Laden eines oder mehrerer Dateien:",
           projectsResponse.status,
           aboutResponse.status,
-          clientsResponse.status
+          clientsResponse.status,
+          footerResponse.status,
         );
         return false;
       }
@@ -52,6 +59,7 @@ const dataStore = {
       const projectsData = await projectsResponse.json();
       const aboutData = await aboutResponse.json();
       const clientsData = await clientsResponse.json();
+      const footerData = await footerResponse.json();
 
       console.log("dataStore: Laden erfolgreich");
 
@@ -59,6 +67,7 @@ const dataStore = {
       this.projectsData = projectsData;
       this.aboutImprintData = aboutData;
       this.clientsData = clientsData;
+      this.footerData = footerData;
 
       return true;
     } catch (error) {
