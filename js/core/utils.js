1c22af005595a9b1b4f871312ada1789f6e409ed
Patrick Richter <p.t.richter@gmail.com>
2025-03-27 14:16:06 +0100

    Error handled: scrolling blocked due to wrong scroll snap (on picture elment)

--- js/core/utils.js
+++ js/core/utils.js
@@ -123,3 +123,12 @@ export function detectWebpSupport() {
   return false;
 }
 
+export function checkImageExists(url) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.onload = () => resolve(true);
+    img.onerror = () => resolve(false);
+    img.src = url;
+  });
+}
+
